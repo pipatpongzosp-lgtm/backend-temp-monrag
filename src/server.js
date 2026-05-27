91342f77d67435bc1562fa35711a7edc6c27141e
pipatpongzosp-lgtm <pipatpongzosp@gmail.com>
2026-05-27 16:04:39 +0700

    add FE vercel

--- src/server.js
+++ src/server.js
@@ -19,6 +19,7 @@ const corsOptions = {
     "http://localhost:5173",
     "http://localhost:5174",
     "http://localhost:5175",
+    "https://fe-template-3634zb01o-pipatpongzosp-lgtms-projects.vercel.app"
   ], // frontend domain
   credentials: true, // ✅ allow cookies to be sent
 };
